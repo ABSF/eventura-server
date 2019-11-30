7a271c360c710027dd38ee0b7bbf11a74728164b
Filip <kam.filip@gmail.com>
2019-11-30 17:22:32 +0100

    Project template

---
+++ src/main/java/pl/eventura/eventuraserver/EventuraServerApplication.java
@@ -0,0 +1,13 @@
+package pl.eventura.eventuraserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventuraServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventuraServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/eventura/eventuraserver/EventuraServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.eventura.eventuraserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventuraServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
