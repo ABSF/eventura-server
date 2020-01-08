9d4e21d31a743eae65a405f8ccc58c968ade0f71
Filip <kam.filip@gmail.com>
2020-01-09 00:32:41 +0100

    Add simple find by name

--- src/main/java/pl/eventura/eventuraserver/events/EventRepository.java
+++ src/main/java/pl/eventura/eventuraserver/events/EventRepository.java
@@ -14,4 +14,6 @@ public interface EventRepository extends JpaRepository<Event, Integer> {
     @Query(value = "SELECT e FROM Event e WHERE ((:text is null or lower(e.name) LIKE lower(CONCAT('%',:text,'%'))) or (:text is null or lower(e.summary) LIKE lower(CONCAT('%',:text,'%')))) and (CAST(:dateStart as date) is null or CAST(:dateEnd as date) is null or e.date >= CAST(:dateStart as date) and e.date <= CAST(:dateEnd as date)) order by e.date asc")
     List<Event> findByNameOrSummaryContainingOrDateIsBetween(String text, LocalDate dateStart, LocalDate dateEnd);
 
+    List<Event> findByNameContaining(String name);
+
 }
